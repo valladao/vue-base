4d3113f3c719d563fcd659b02e940efc2e9dbba0
valladao <valladao@gmail.com>
2020-12-12 18:46:18 -0300

    Change router file location

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import router from './router'
+import vuetify from './plugins/vuetify'
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 
