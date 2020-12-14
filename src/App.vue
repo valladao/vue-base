25b6c1be7a776c1b8d8c3fc57452c4019acbb016
valladao <valladao@gmail.com>
2020-12-13 21:04:29 -0300

    Vuetify basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view/>
-	</div>
+	<!-- Financial TerraCotta Application -->
+	<v-app>
+		<v-navigation-drawer
+			v-model="drawer"
+			app
+		>
+			<v-list-item to="/">
+				<v-list-item-content>
+					<v-list-item-title>Link 1</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+			<v-list-item to="/about">
+				<v-list-item-content>
+					<v-list-item-title>Link 2</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-navigation-drawer>
+		<v-app-bar app>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+		</v-app-bar>
+		<v-main>
+			<v-container fluid>
+				<router-view/>
+			</v-container>
+		</v-main>
+		<v-footer app></v-footer>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+<script>
+export default {
+	data: () => ({
+		drawer: true
+	})
 }
-
-#nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
-}
-</style>
+</script>
